7de1213a31c985a794434e120f3e67268b0dee28
Tahir <Tahirhussain32145@gmail.com>
2022-10-10 11:07:04 +0545

    After createpost backend completion

--- server/controller/postsController.js
+++ server/controller/postsController.js
@@ -1,11 +1,28 @@
+const PostMessage = require('../model/postMessageModel')
 
 
-const getPosts = (req,res) => {
-    res.json('Working')
+const getPosts = async(req,res) => {
+     try {
+        const posts = await PostMessage.find()
+        res.status(200).json(posts)
+     } catch (error) {
+        res.status(404).json({message: error.message})
+     }
 }
 
-const createPost = (req,res) => {
-    res.json('Working')
+const createPost = async(req,res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+    
+    try {
+    await newPost.save() 
+    res.status(201).json(newPost)  
+        
+    } catch (error) {
+        res.status(404).json({message: error.message})
+        
+    }
 
 }
 
